f272d24df7ab261163d2892436239851548a1db1
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-10-02 16:45:28 +0530

    Update EndPoints.js

--- src/API/EndPoints.js
+++ src/API/EndPoints.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://blogger-backend-jobt.onrender.com
-",
+  baseURL: "https://blogger-backend-jobt.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
