70e906f66ea3b856aa375fddcf7e235e39fc178c
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-10-08 10:30:08 +0530

    Update CreateBlog.jsx

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-//import { post } from '../API/EndPoints';
+import { post } from '../API/EndPoints';
 import { toast } from 'react-hot-toast';
-import axios from 'axios'
 
 const CreateBlog = () => {
     const [blogData, setBlogData] = useState({
@@ -27,7 +26,7 @@ const CreateBlog = () => {
         };
 
         try {
-            await axios.post('https://blogger-backend-jobt.onrender.com/api/v1/blog/create-blogs', formData);
+            await post('/api/v1/blog/create-blogs', formData);
             toast.success('Blog created successfully!');
             navigate('/');
         } catch (error) {
