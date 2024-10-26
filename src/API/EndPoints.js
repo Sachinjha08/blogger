273b268da6d7a65fee95b05fa8ec5aca4b133602
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-10-26 16:51:23 +0530

    Update EndPoints.js

--- src/API/EndPoints.js
+++ src/API/EndPoints.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
+export const BaseUrl = "http://localhost:8000";
+
 const instance = axios.create({
-  baseURL: "https://blogger-backend-jobt.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: BaseUrl,
   withCredentials: true,
 });
-
 export const get = (url, params) => instance.get(url, { params });
 export const post = (url, data) => instance.post(url, data);
-export const put = (url, data) => instance.put(url, data);
+export const path = (url, data) => instance.patch(url, data);
 export const dele = (url) => instance.delete(url);
