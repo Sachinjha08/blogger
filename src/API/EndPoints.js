d68ed054a1f195aa2d8715d0008c7b9447618b13
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-10-26 16:59:36 +0530

    Update EndPoints.js

--- src/API/EndPoints.js
+++ src/API/EndPoints.js
@@ -9,5 +9,5 @@ const instance = axios.create({
 
 export const get = (url, params) => instance.get(url, { params });
 export const post = (url, data) => instance.post(url, data);
-export const path = (url, data) => instance.patch(url, data);
+export const patch = (url, data) => instance.patch(url, data); 
 export const dele = (url) => instance.delete(url);
